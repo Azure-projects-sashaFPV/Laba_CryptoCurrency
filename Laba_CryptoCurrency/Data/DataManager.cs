6a07d147193b772c23be0969a5967e44ad4606eb
sashaFPV <andrei.cazanov2017@gmail.com>
2022-01-16 10:31:39 +0200

    Добавьте файлы проекта.

---
+++ Laba_CryptoCurrency/Data/DataManager.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Laba_CryptoCurrency.Models;
+
+namespace Laba_CryptoCurrency.Data
+{
+    public class DataManager : DbContext
+    {
+        public DataManager() : base()
+        { }
+        public DataManager(DbContextOptions options) : base(options)
+        { }
+        public DbSet<CryptoCurrency> CryptoCurrencies { get; set; }
+    }
+}
---
+++ Laba_CryptoCurrency/Models/CryptoCurrency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laba_CryptoCurrency.Models
+{
+    public class CryptoCurrency
+    {
+        public int Id { get; set; }
+        public string CryptoPair { get; set; }
+        public string Interval { get; set; }
+        public List<double> Prices { get; set; } 
+    }
+}
